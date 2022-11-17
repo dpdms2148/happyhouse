5e9a864c5b5aeb1a5d95cd6049a2fe373da04cbe
dpdms2148 <dpdms2148@naver.com>
2022-11-17 15:25:32 +0900

    add join form

--- HappyHouse_Front_구미_05반_이재완_장예은/src/components/user/UserLogin.vue
+++ HappyHouse_Front_구미_05반_이재완_장예은/src/components/user/UserLogin.vue
@@ -10,8 +10,7 @@
       <b-col cols="8">
         <b-card class="text-center mt-3" style="max-width: 40rem" align="left">
           <b-form class="text-left">
-            <b-alert show variant="danger" v-if="isLoginError">아이디 또는 비밀번호를 확인하세요.</b-alert>
-            <b-form-group label="아이디:" label-for="userid">
+            <b-form-group label-cols-lg="3" label="아이디:" label-for="userid">
               <b-form-input
                 id="userid"
                 v-model="user.userid"
@@ -20,7 +19,7 @@
                 @keyup.enter="confirm"
               ></b-form-input>
             </b-form-group>
-            <b-form-group label="비밀번호:" label-for="userpwd">
+            <b-form-group label-cols-lg="3" label="비밀번호:" label-for="userpwd">
               <b-form-input
                 type="password"
                 id="userpwd"
@@ -30,8 +29,25 @@
                 @keyup.enter="confirm"
               ></b-form-input>
             </b-form-group>
-            <b-button type="button" variant="primary" class="m-1" @click="confirm">로그인</b-button>
-            <b-button type="button" variant="success" class="m-1" @click="movePage">회원가입</b-button>
+
+            <b-alert show variant="danger" v-if="isLoginError"
+              >아이디 또는 비밀번호를 확인하세요.</b-alert
+            >
+
+            <b-button
+              type="button"
+              variant="primary"
+              class="m-1"
+              @click="confirm"
+              >로그인</b-button
+            >
+            <b-button
+              type="button"
+              variant="success"
+              class="m-1"
+              @click="movePage"
+              >회원가입</b-button
+            >
           </b-form>
         </b-card>
       </b-col>
