cc6b361bf511cc6eb10d3bcded1a01542f428fe6
dpdms2148 <dpdms2148@naver.com>
2022-11-21 13:31:44 +0900

    add userinfo modify form

--- HappyHouse_Front_구미_05반_이재완_장예은/src/components/user/UserMyPage.vue
+++ HappyHouse_Front_구미_05반_이재완_장예은/src/components/user/UserMyPage.vue
@@ -9,45 +9,99 @@
       <b-col></b-col>
       <b-col cols="8">
         <b-table-simple hover>
+          <colgroup>
+            <col style="width: 130px" />
+            <col style="" />
+            <col style="width: 140px" />
+          </colgroup>
           <b-tbody>
             <b-tr>
               <b-th>아이디</b-th>
               <b-td>{{ userInfo.userid }}</b-td>
-              <b-td colspan="2"></b-td>
+              <b-td></b-td>
             </b-tr>
 
             <b-tr>
               <b-th scope="row">이름</b-th>
               <b-td>{{ userInfo.username }}</b-td>
-              <b-td colspan="2">
-                <b-button variant="outline-dark" size="sm">이름 변경</b-button>
-              </b-td>
+              <b-td></b-td>
             </b-tr>
 
-            <b-tr>
+            <b-tr v-show="isPwdShow">
               <b-th scope="row">비밀번호</b-th>
               <b-td>********</b-td>
-              <b-td colspan="2">
-                <b-button variant="outline-dark" size="sm">
+              <b-td>
+                <b-button
+                  variant="outline-dark"
+                  size="sm"
+                  @click="isPwdShow = !isPwdShow"
+                  block
+                >
                   비밀번호 변경
                 </b-button>
               </b-td>
             </b-tr>
 
-            <b-tr>
+            <b-tr v-show="!isPwdShow">
+              <b-th scope="row">비밀번호</b-th>
+              <b-td>
+                <b-input
+                  type="email"
+                  placeholder="변경할 비밀번호를 입력해주세요"
+                ></b-input>
+                <b-button
+                  variant="light"
+                  type="button"
+                  @click="isPwdShow = !isPwdShow"
+                  class="m-1"
+                  >취소</b-button
+                >
+                <b-button variant="dark" type="button" class="m-1"
+                  >완료</b-button
+                >
+              </b-td>
+              <b-td></b-td>
+            </b-tr>
+
+            <b-tr v-show="isEmailShow">
               <b-th scope="row">이메일</b-th>
               <b-td>{{ userInfo.email }}</b-td>
-              <b-td colspan="2">
-                <b-button variant="outline-dark" size="sm">
-                  이메일 변경
-                </b-button>
+              <b-td>
+                <b-button
+                  variant="outline-dark"
+                  size="sm"
+                  @click="isEmailShow = !isEmailShow"
+                  block
+                  >이메일 변경</b-button
+                >
               </b-td>
+            </b-tr>
+
+            <b-tr v-show="!isEmailShow">
+              <b-th scope="row">이메일</b-th>
+              <b-td>
+                <b-input
+                  type="email"
+                  placeholder="변경할 이메일을 입력해주세요"
+                ></b-input>
+                <b-button
+                  variant="light"
+                  type="button"
+                  @click="isEmailShow = !isEmailShow"
+                  class="m-1"
+                  >취소</b-button
+                >
+                <b-button variant="dark" type="button" class="m-1"
+                  >완료</b-button
+                >
+              </b-td>
+              <b-td></b-td>
             </b-tr>
 
             <b-tr>
               <b-th scope="row">가입일</b-th>
               <b-td>{{ userInfo.joindate }}</b-td>
-              <b-td colspan="2"></b-td>
+              <b-td></b-td>
             </b-tr>
           </b-tbody>
         </b-table-simple>
@@ -58,17 +112,24 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 
-const memberStore = 'memberStore'
+const memberStore = "memberStore";
 
 export default {
-  name: 'UserMyPage',
+  name: "UserMyPage",
   components: {},
   computed: {
-    ...mapState(memberStore, ['userInfo']),
+    ...mapState(memberStore, ["userInfo"]),
   },
-}
+  data() {
+    return {
+      isNameShow: true,
+      isPwdShow: true,
+      isEmailShow: true,
+    };
+  },
+};
 </script>
 
 <style>
