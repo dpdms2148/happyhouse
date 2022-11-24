2107571724a335130e18580752ec02258b83ea71
dpdms2148 <dpdms2148@naver.com>
2022-11-24 16:04:23 +0900

    change main page
    Former-commit-id: e5929abd3d30ed66490380341a4ebf41e90ee834

--- HappyHouse_Front_구미_05반_이재완_장예은/src/views/AppMain.vue
+++ HappyHouse_Front_구미_05반_이재완_장예은/src/views/AppMain.vue
@@ -1,9 +1,16 @@
 <template>
   <b-container fluid class="px-0 background">
+    <b-row class="mt-5">
+      <b-col></b-col>
+      <b-col>
+        <div class="text-white title">HAPPY HOUSE</div>
+      </b-col>
+      <b-col></b-col>
+    </b-row>
     <b-row>
-      <b-col ></b-col>
+      <b-col></b-col>
       <b-col cols="6" class="mt-5">
-        <b-jumbotron  
+        <b-jumbotron
           bg-variant="muted"
           text-variant="dark"
           border-variant="dark"
@@ -24,21 +31,54 @@
                 placeholder="기사 검색"
               ></b-form-input>
             </b-input-group>
-            <b-table striped hover :items="items" @row-selected="onRowSelected">
+            <!-- <b-table striped hover :items="items" @row-selected="onRowSelected">
               <div style="width: 100%; height: 350px; overflow: auto"></div>
-            </b-table>
-            <!-- <b-row border-variant="dark"><a href="https://n.news.naver.com/article/001/0013594370?sid=101">[1] 1인가구 늘고 금리 오르자 초소형 아파트 매입비중 역대 최고</a></b-row> -->
-            <!--<b-row><a href="https://n.news.naver.com/article/243/0000034834?sid=101">[2] “나혼자 산다” 초소형 아파트 매매 비중 역대 최대</a></b-row>
-        <b-row><a href="https://n.news.naver.com/article/009/0005049372?sid=101">[3] 부동산 얼어붙는데 이건 뜨네...매입비율 역대최고 ‘초소형 아파트’</a></b-row>
-        <b-row><a href="https://n.news.naver.com/article/081/0003319280?sid=101">[4] 공시가 2020년 수준으로 낮춘다… 9억 이상 아파트 최대 수혜</a></b-row>
-        <b-row><a href="https://n.news.naver.com/article/016/0002069339?sid=101">[5] 내년 아파트 공시가 현실화율 69%로…2020년 수준으로 낮춘다 [부동산360]</a></b-row> -->
+            </b-table> -->
+            <b-list-group class="text-left">
+              <b-list-group-item
+                ><a
+                  class="text-dark"
+                  href="https://n.news.naver.com/article/001/0013594370?sid=101"
+                  >[1] 1인가구 늘고 금리 오르자 초소형 아파트 매입비중 역대
+                  최고</a
+                ></b-list-group-item
+              >
+              <b-list-group-item
+                ><a
+                  class="text-dark"
+                  href="https://n.news.naver.com/article/243/0000034834?sid=101"
+                  >[2] “나혼자 산다” 초소형 아파트 매매 비중 역대 최대</a
+                ></b-list-group-item
+              >
+              <b-list-group-item
+                ><a
+                  class="text-dark"
+                  href="https://n.news.naver.com/article/009/0005049372?sid=101"
+                  >[3] 부동산 얼어붙는데 이건 뜨네...매입비율 역대최고 ‘초소형
+                  아파트’</a
+                ></b-list-group-item
+              >
+              <b-list-group-item
+                ><a
+                  class="text-dark"
+                  href="https://n.news.naver.com/article/081/0003319280?sid=101"
+                  >[4] 공시가 2020년 수준으로 낮춘다… 9억 이상 아파트 최대
+                  수혜</a
+                ></b-list-group-item
+              >
+              <b-list-group-item
+                ><a
+                  class="text-dark"
+                  href="https://n.news.naver.com/article/016/0002069339?sid=101"
+                  >[5] 내년 아파트 공시가 현실화율 69%로…2020년 수준으로 낮춘다
+                  [부동산360]</a
+                ></b-list-group-item
+              >
+            </b-list-group>
           </b-jumbotron>
-          <!-- </b-col> -->
         </b-jumbotron>
-
-        <!-- </b-col> -->
       </b-col>
-      <b-col ></b-col>
+      <b-col></b-col>
     </b-row>
   </b-container>
 </template>
@@ -79,5 +119,9 @@ export default {
   background: url("@/assets/happyhousemain.png") no-repeat center;
   background-size: cover;
   overflow: hidden;
+}
+.title {
+  font-size: 70px;
+  font-family: "Times New Roman", Times, serif;
 }
 </style>
